7a13807b12beb9abe2fbce5c1f5b36eed3ccb64b
Nevena Kicanovic <nevena@contactus.se>
2026-08-27 10:20:28 +0200

    lägg till class .cool på alla li elements

--- Lektion7-exempel/Example 3_DOM/js/example.js
+++ Lektion7-exempel/Example 3_DOM/js/example.js
@@ -6,4 +6,15 @@ let newItemLast = document.createElement('li');             //Skapar li element
 let newTextLast = document.createTextNode('cream');         //Skapar text node
 newItemLast.appendChild(newTextLast);                       //Lägg till text node i li element
 list.appendChild(newItemLast);                              //Lägg till li element i slutet av lista
+
 //...på start
+let newItemFirst = document.createElement('li');            //Skapar li element
+let newTextFirst = document.createTextNode('kale');         //Skapar text node
+newItemFirst.appendChild(newTextFirst);                     //Lägg till text node i li element
+list.insertBefore(newItemFirst, list.firstChild)            //Lägg till li element i början av lista
+
+//lägg till class .cool på alla li elements
+let listElements = document.querySelectorAll('li');         //Select all li elements
+for(i=0; i<listElements.length; i++) {                      //Gå genom alla element i listan
+    listElements[i].className = 'cool';                     // byta class till .cool class
+}
