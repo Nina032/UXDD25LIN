59f7b423270cec56acad9566d05dca451fd2c50b
Nevena Kicanovic <nevena@contactus.se>
2026-08-27 10:35:47 +0200

    lägg till räknare i heading

--- Lektion7-exempel/Example 3_DOM/js/example.js
+++ Lektion7-exempel/Example 3_DOM/js/example.js
@@ -13,8 +13,17 @@ let newTextFirst = document.createTextNode('kale');         //Skapar text node
 newItemFirst.appendChild(newTextFirst);                     //Lägg till text node i li element
 list.insertBefore(newItemFirst, list.firstChild)            //Lägg till li element i början av lista
 
+
+
 //lägg till class .cool på alla li elements
 let listElements = document.querySelectorAll('li');         //Select all li elements
-for(i=0; i<listElements.length; i++) {                      //Gå genom alla element i listan
+let totalItems = listElements.length;
+for(i=0; i<totalItems; i++) {                               //Gå genom alla element i listan
     listElements[i].className = 'cool';                     // byta class till .cool class
 }
+
+//lägg till räknare i heading
+let heading = document.querySelector('h2');                         //
+let headingText = heading.firstChild.nodeValue;                     //
+let newHeading = headingText + '<span>' + totalItems +'</span>';
+heading.innerHTML = newHeading;
