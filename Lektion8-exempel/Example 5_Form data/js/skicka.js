e3a56ca5fce12095b8f3c7e753754886a4e296d6
Nevena Kicanovic <nevena@contactus.se>
2026-08-31 10:47:38 +0200

    info om jquery

---
+++ Lektion8-exempel/Example 5_Form data/js/skicka.js
@@ -0,0 +1,27 @@
+/* https://jquery.com/
+jQuery är ett snabbt, litet och funktionsrikt JavaScript-bibliotek. 
+Det gör saker som HTML-dokumentgenomgång och manipulering, händelsehantering, 
+animering och Ajax mycket enklare med ett lättanvänt API 
+som fungerar i en mängd webbläsare. 
+Med en kombination av mångsidighet och utbyggbarhet har jQuery förändrat 
+hur miljontals människor skriver JavaScript.
+
+
+https://www.w3schools.com/jquery/jquery_syntax.asp
+
+  Basic syntax is: $(selector).action()
+
+    A $ sign to define/access jQuery
+    A (selector) to "query (or find)" HTML elements
+    A jQuery action() to be performed on the element(s)
+
+    Examples:
+
+      $(this).hide() - hides the current element.
+
+      $("p").hide() - hides all <p> elements.
+
+      $(".test").hide() - hides all elements with class="test".
+
+      $("#test").hide() - hides the element with id="test".
+*/
