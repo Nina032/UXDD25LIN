aac1f285129ab0be3ab5b275171e0df50e727bda
Nevena Kicanovic <nevena@contactus.se>
2026-08-31 10:27:41 +0200

    recorderControls, record function och eventlistener

--- Lektion8-exempel/Example 4_Events/js/example.js
+++ Lektion8-exempel/Example 4_Events/js/example.js
@@ -14,7 +14,28 @@ function writeLabel(e){
     noteName.textContent = textEntered;
 }
 
+function recorderControls(e) {
+    //Fånga target
+    target = e.target;
+    
+    //Avbryta default action
+    if(e.preventDefault) {
+        e.preventDefault();
+    }
 
+    //Bestäma data-state
+    switch (target.getAttribute('data-state')) {
+        case 'record':
+            record(target);
+    }
 
+}
+function record(target) {
+    target.setAttribute('data-state','stop');
+    target.textContent = 'stop';
+}
+
+//Eventlistener på document
+document.addEventListener('click', function(e) {recorderControls(e)});
 
 noteInput.addEventListener('input', writeLabel);    //Event listener på input för att lysna efter input, kallar function writelabel 
