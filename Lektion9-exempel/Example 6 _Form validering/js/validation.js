09da6e52ea362df6e1b9637f685c54c3a5308b39
Nevena Kicanovic <nevena@contactus.se>
2026-09-03 11:24:57 +0200

    Plan för validation script (utan HTML validation)

---
+++ Lektion9-exempel/Example 6 _Form validering/js/validation.js
@@ -0,0 +1,7 @@
+//JavaScript validation av subscription formulär (utan HTML validation)
+
+//A. Anonym funktion som kallas av submit event
+    //B. Funktion som utför generiska kontroll i funktion A
+    //C. Funktion som utför specifika kontrol i funktion A
+//D. Funktion som ska hämta/uppdatera/visa/dölja felmendelande
+//E. Objekt som kontrollerar datatyp med RegEx
