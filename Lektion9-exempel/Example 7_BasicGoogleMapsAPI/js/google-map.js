3eb57675cd118e18cbff015296ed0bf3933efd28
Nevena Kicanovic <nevena@contactus.se>
2026-09-03 10:02:54 +0200

    google maps init och load

--- Lektion9-exempel/Example 7_BasicGoogleMapsAPI/js/google-map.js
+++ Lektion9-exempel/Example 7_BasicGoogleMapsAPI/js/google-map.js
@@ -1,4 +1,21 @@
+function init() {
+let mapOptions = {
+    center: new google.maps.LatLng(40.782710,-73.965310),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 13
+};
+let venueMap;
+venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+}
+
+function loadScript() {
+    let script = document.createElement('script');      //Skapar <script> element
+    script.src ='http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
+    document.body.appendChild(script);                  //lägger till script element som sista i body
+}
+
+window.onload = loadScript;     //on load anrop till loadScript
 
 //http://maps.googleapis.com/maps/api/js?sensor=false&callback=init
 
