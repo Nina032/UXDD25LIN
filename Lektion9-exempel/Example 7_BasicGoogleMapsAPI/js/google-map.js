923633cd7355af33824e737e272b3c675ee498a6
Nevena Kicanovic <nevena@contactus.se>
2026-09-03 09:36:19 +0200

    google apis länkar

---
+++ Lektion9-exempel/Example 7_BasicGoogleMapsAPI/js/google-map.js
@@ -0,0 +1,6 @@
+
+
+//http://maps.googleapis.com/maps/api/js?sensor=false&callback=init
+
+//https://developers.google.com/maps/documentation/javascript/add-google-map
+//https://mapsplatform.google.com/maps-demo-key/
